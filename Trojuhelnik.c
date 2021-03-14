cac68be2090f0a6bb14b0d09d26b39a0148d4f7c
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2021-03-14 10:18:26 +0100

    Create Trojuhelnik.c

---
+++ Trojuhelnik.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    /* trojuhelnik, nevim jak udelat mezeru tak je tam tečka*/
+    int i, j;
+    int h;
+    scanf("%d", &h);
+    
+    for (i = 1; i <= h; i++)
+    {
+        for (j = 1; j <= h - i; j++) 
+            printf(".  ");
+        for (j = 2; j <= i * 2; j++)    
+            printf("*");
+        printf("\n");
+    }
+   
+    return 0;
+}
