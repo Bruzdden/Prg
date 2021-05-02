71c0b5799062ed5d903dd8ab67f7dc46e682cfe1
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2021-05-02 17:09:53 +0200

    Create round_floats.c

---
+++ round_floats.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+void generate_items(void);
+    srand((int)time(0));
+
+    float a = 5.0;
+    
+    
+{       
+void show_items(void);
+    for (int i=0;i<10;i++)
+        printf("%.1f\n", ((float)rand()/(float)(RAND_MAX)) * a);
+
+   
+    return 0;
+
+
+}}
